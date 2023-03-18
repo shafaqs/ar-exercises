f7e73b446260736ecb74bf76433fbe4d55a133c2
Shafaq Saud <shafaq.saud@gmail.com>
2023-03-18 18:56:24 -0400

    Exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# prompt the user for a store name
+print "Enter store name: "
+store_name = gets.chomp
+
+# create a new store with the given name
+store = Store.new(name: store_name)
+
+# attempt to save the new store
+if store.save
+  puts "Store created successfully!"
+else
+  # display any error messages returned by ActiveRecord
+  store.errors.full_messages.each do |message|
+    puts message
+  end
+end
+
